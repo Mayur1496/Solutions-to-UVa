50d60f32a7a5cf183ded39961dc021077388f004
Mayur1496 <mayur.1496@gmail.com>
2017-09-17 23:19:19 +0530

    corrected 119 endl error

--- 119.cpp
+++ 119.cpp
@@ -3,10 +3,9 @@
 #include <map>
 using namespace std;
 int main() {
-  int n,money,t;
+  int n,money,t,temp=0;
   string name, frnd;
   vector <string> names;
-  cin>>n;
   while(cin>>n){
     name.clear();
     map<string, int> map;
@@ -17,17 +16,21 @@ int main() {
     }
     for (int i = 0; i < n; i++) {
       cin>>name>>money>>t;
-      map[name]+=money%t;
-      map[name]-=money;
-      for (int j = 0; j < t; j++) {
-        cin>>frnd;
-        map[frnd]+=money/t;
+      if(t>0){
+        map[name]+=money%t;
+        map[name]-=money;
+        for (int j = 0; j < t; j++) {
+          cin>>frnd;
+          map[frnd]+=money/t;
+        }
       }
+    }
+    if (temp>0) {
+      std::cout << '\n';
     }
     for (int i = 0; i < n; i++) {
       cout<<names[i]<<" "<<map[names[i]]<<endl;
     }
-    cout<<endl;
   }
   return 0;
 }
